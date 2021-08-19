59189a2fb2aacca8045ca96ba35c063236c076f3
Alicia Stotz <aliciastotz@gmail.com>
2021-08-19 16:11:08 +0200

    feat(sectionList): add subsection + manage location

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Navbar } from '../shared/Navbar';
-import { SummaryList } from '../shared/SummaryList';
+import { SectionList } from '../shared/SectionList';
 
 import { Introduction } from '../routes/Introduction/Introduction';
 import { Example } from '../routes/Example/Example';
@@ -10,23 +10,22 @@ import { Example } from '../routes/Example/Example';
 
 const App: React.FC = () => {
   return (
-    <div className="App" style={{ height: '100vh' }}>
-      <Navbar />
-      <div className="row h-100">
-        <div className="col-3 py-4" style={{ paddingRight: 0 }}>
-          <SummaryList />
-        </div>
-        <div className="col-9 py-4 bg-light">
-          <Router>
+    <Router>
+      <div className="App" style={{ height: '100vh' }}>
+        <Navbar />
+        <div className="row h-100">
+          <div className="col-3 py-4" style={{ paddingRight: 0 }}>
+            <SectionList />
+          </div>
+          <div className="col-9 py-4 bg-light">
             <Switch>
               <Route exact path="/" component={Introduction}></Route>
               <Route path="/example" component={Example}></Route>
             </Switch>
-          </Router>
+          </div>
         </div>
       </div>
-
-    </div>
+    </Router>
   );
 }
 
---
+++ src/components/shared/SectionList.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+interface ISection {
+  id: string;
+  link: string;
+  title: string;
+  label: string;
+}
+interface ISummaryList extends ISection {
+  subSection?: ISection[];
+}
+
+const list: ISummaryList[] = [
+  {
+    id: 'introduction',
+    link: '/',
+    title: 'Introduction section',
+    label: 'Introduction'
+  },
+  {
+    id: 'example',
+    link: '/example',
+    title: 'Example section',
+    label: 'Exemple',
+    subSection: [
+      {
+        id: 'defaultPokemonCard',
+        link: '/example/default',
+        title: 'Default Pokemon card',
+        label: 'Card Pokemon',
+      }
+    ]
+  }
+];
+
+export const SectionList = () => {
+  const location = useLocation();
+
+  const activeLink = (link: string, index: number): boolean => {
+    const pathName = location.pathname.split("/");
+    const splitedLink = link.split("/");
+
+    if (pathName[index] === "" && link === "/") {
+      return true;
+    }
+    return !!(pathName[index] && pathName[index] === splitedLink[index]);
+  }
+
+  return <ul className="list-group list-group-flush list-group-numbered">
+    {
+      list.map((item: ISummaryList) =>
+        <React.Fragment key={item.id}>
+          <a
+            href={item.link}
+            title={item.title}
+            className={`list-group-item list-group-item-action${activeLink(item.link, 1) ? " list-group-item-secondary" : ""}${item.subSection ? " border-0" : ""}`}>
+            {item.label}
+
+          </a>
+          {
+            item.subSection ?
+              <ul className="list-group list-group-flush">
+                {
+                  item.subSection.map((section: ISection) =>
+                    <a
+                      key={`${item.id}-${section.id}`}
+                      href={section.link}
+                      style={{ fontWeight: "lighter" }}
+                      title={section.title}
+                      className={`list-group-item font-weight-light px-4${activeLink(section.link, 2) ? ' text-primary' : ''}`}>
+                      {section.label}
+                    </a>
+                  )
+                }
+
+              </ul>
+              : null
+          }
+
+        </React.Fragment>
+      )
+    }
+
+  </ul>
+};
