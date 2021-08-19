d8b720324dff22c015cd9a71464713de3adc3d25
Alicia Stotz <aliciastotz@gmail.com>
2021-08-19 09:30:10 +0200

    feat(pokemonCard): add tests

--- src/components/routes/Example/PokemonCard.spec.tsx
+++ src/components/routes/Example/PokemonCard.spec.tsx
@@ -12,7 +12,24 @@ const defaultProps: IPokemonCardProps = {
     height: 15,
     order: 1,
     weight: 9,
-    base_experience: 55
+    base_experience: 55,
+    types: [
+      {
+        name: "test type",
+        url: "https://test/"
+      }
+    ]
+  }
+};
+
+const defaultPropsWithoutTypes: IPokemonCardProps = {
+  pokemon: {
+    id: 1,
+    name: "test 2",
+    height: 18,
+    order: 3,
+    weight: 12,
+    base_experience: 64
   }
 };
 
@@ -23,8 +40,37 @@ describe("Pokemon card component", () => {
     });
 
     it("Should display card", () => {
-      screen.debug();
+      //screen.debug();
       expect(screen.getByText('test')).toBeInTheDocument();
+    });
+
+    it("Should display table information", () => {
+      expect(screen.getByText('15')).toBeInTheDocument();
+      expect(screen.getByText('9')).toBeInTheDocument();
+      expect(screen.getByText('55')).toBeInTheDocument();
+    });
+
+    // @TODO: Snippet "console.log(screen.getByRole("")
+    // @TODO: Snippet "expect(screen.getByRole('link')).toBeInTheDocument();"
+    it("Should display button information", () => {
+      expect(screen.getByRole('link')).toBeInTheDocument();
+    });
+
+    // @TODO: Snippet "expect(screen.getByRole('button', {name: "Show more information"})).toBeInTheDocument();"
+    it("Should display more information button", () => {
+      expect(screen.getByRole('button', { name: "Show more information" })).toBeInTheDocument();
+    })
+  });
+
+  describe("Pokemon card without types", () => {
+    beforeEach(() => {
+      render(<PokemonCard {...defaultPropsWithoutTypes} />);
+    });
+
+    // @TODO: Snippet "expect(screen.getByRole('button', {name: "Show more information"})).toBeNull();"
+    it("Should not display more information button", () => {
+      //expect(screen.getByRole('button', { name: "Show more information" })).toBeNull();
+      expect(screen.queryByRole('button', { name: "Show more information" })).toBeNull();
     })
   })
 })
