a5322a809ca330641a24f66fa028d4d9595cbd82
Alicia Stotz <aliciastotz@gmail.com>
2021-09-03 10:28:35 +0200

    feat(toolBar): add fakePromise

--- src/components/routes/Example/GameZone.tsx
+++ src/components/routes/Example/GameZone.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import { Toolbar } from './ToolBar';
+import { ToolBar } from './ToolBar';
 import { SummaryButton } from '../../shared/SummaryButton';
 
 const SUMMARY_CONTENT: JSX.Element = <div></div>
 export const GameZone = () => {
   return <div className="container row">
     <SummaryButton content={SUMMARY_CONTENT} />
-    <Toolbar numberOfPokemon={0} />
+    <ToolBar numberOfPokemon={0} withFakePromise />
   </div>
 }
--- src/components/routes/Example/ToolBar.tsx
+++ src/components/routes/Example/ToolBar.tsx
@@ -3,28 +3,43 @@ import AddIcon from '@material-ui/icons/Add';
 
 export interface IToolBarProps {
   numberOfPokemon: number;
+  withFakePromise?: boolean;
 }
 
-export const Toolbar = ({ numberOfPokemon }: IToolBarProps) => {
+export const ToolBar = ({ numberOfPokemon, withFakePromise }: IToolBarProps) => {
   const [pokeball, setPokeball] = React.useState<number>(0);
   const [inputPokeball, setInputPokeball] = React.useState<number>(0);
 
-  const typePokeballHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+  const typePokeballHandler = (e: React.KeyboardEvent<HTMLInputElement>): void => {
     setInputPokeball(parseInt(e.currentTarget.value, 10));
   }
 
-  const preventKeyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+  const preventKeyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>): void => {
     if (e.key === 'e' || e.key === ',' || e.key === '.' || e.key === ';') {
       e.preventDefault();
     }
   }
 
+  const fakePromise = (pokeballNumber: number): Promise<number> => {
+    return Promise.resolve(pokeballNumber);
+  }
+
+  const onClickWithPromise = async (pNumber: number) => {
+    const pokeballNumber = await fakePromise(pNumber);
+    setPokeball(pokeball + pokeballNumber);
+  }
+
+
   return <div className="tool-bar card">
     <div className="d-flex justify-content-between py-2 align-items-center">
       <div className="">
-        <span className="me-2">Pokedex: <strong>{numberOfPokemon}</strong></span>
+        <span className="me-2" title="Number of pokemon in pokedex">
+          Pokedex: <strong>{numberOfPokemon}</strong>
+        </span>
         <span className="text-secondary">|</span>
-        <span className="ms-2">Pokeball: <strong>{pokeball}</strong></span>
+        <span className="ms-2" title="Number of pokeball">
+          Pokeball: <strong>{pokeball}</strong>
+        </span>
       </div>
       <div className="input-group w-50">
         <input
@@ -37,11 +52,14 @@ export const Toolbar = ({ numberOfPokemon }: IToolBarProps) => {
           min="0"
           onKeyDown={preventKeyDownHandler}
           value={inputPokeball || ""}
-          placeholder="ex: 5" />
+          placeholder="ex : 5" />
         <button
           className="btn btn-outline-secondary btn-sm"
           title="Add number of Pokeball write in input"
-          onClick={() => inputPokeball ? setPokeball(pokeball + inputPokeball) : {}}
+          disabled={!inputPokeball}
+          onClick={() => inputPokeball ?
+            (withFakePromise ? onClickWithPromise(inputPokeball) : setPokeball(pokeball + inputPokeball))
+            : {}}
           type="button">
           Ajouter les Pokeball
         </button>
