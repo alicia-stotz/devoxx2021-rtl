a751fbe31f1813569b90b4b7c2268a617b4eda06
Alicia Stotz <aliciastotz@gmail.com>
2021-09-03 16:08:28 +0200

    feat(gameZone): add summary

--- src/components/routes/Example/GameZone.tsx
+++ src/components/routes/Example/GameZone.tsx
@@ -22,7 +22,54 @@ const POKEMON: IPokemon[] = [
   }
 ]
 
-const SUMMARY_CONTENT: JSX.Element = <div></div>
+const SUMMARY_CONTENT: JSX.Element = <div>
+  <ul>
+    <li>
+      <h6>
+        FireEvent{" "}
+        <a href="https://testing-library.com/docs/dom-testing-library/api-events#fireevent">(reference)</a>
+      </h6>
+      <span>Pour simuler une interraction avec le composant</span>
+      <pre className="mt-3"><code>
+        <mark>fireEvent.click(...);</mark>
+        <mark>fireEvent.change(...)</mark>
+      </code></pre>
+    </li>
+    <li>
+      <h6>
+        UserEvent{" "}
+        <a href="https://testing-library.com/docs/ecosystem-user-event">(reference)</a>
+      </h6>
+      <span>RTL apporte une extension pour les évènements des utilisateurs : UserEvent. Elle est plus proche du comportement du browser. Mais elle n'inclus pas encore toutes les fonctionnalités de FireEvent </span>
+      <pre className="mt-3"><code>
+        <mark>userEvent.type(...);</mark>
+      </code></pre>
+    </li>
+    <li>
+      <h6>
+        FindBy{" "}
+        <a href="https://testing-library.com/docs/queries/about#types-of-queries">(reference)</a>
+      </h6>
+      <span>Utilisé pour les élements de type asynchrone, retourne une Promise</span>
+      <pre className="mt-3"><code>
+        <mark>await screen.findBy(...);</mark>
+      </code></pre>
+    </li>
+    <li>
+      <h6>
+        Fonctions assertive{" "}
+        <a href="https://testing-library.com/docs/queries/about#types-of-queries">(reference)</a>
+      </h6>
+      <span>RTL enrichit les fonctions d'assertion déjà disponible dans Jest.</span>
+      <ul>
+        <li>.toBeDisabled</li>
+        <li>.toBeInvalid</li>
+        <li>.toHaveClass</li>
+        <li>...</li>
+      </ul>
+    </li>
+  </ul>
+</div>
 const TITLE_CONTENT: JSX.Element =
   <div>
     <h3 className="mb-0">Zone de jeu</h3>
