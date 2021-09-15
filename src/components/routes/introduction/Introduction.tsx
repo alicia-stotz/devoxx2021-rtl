e24c26179cbf722dec01316986d4e9c0ee186b9e
Alicia Stotz <aliciastotz@gmail.com>
2021-09-15 15:16:39 +0200

    feat(introduction): review text

--- src/components/routes/introduction/Introduction.tsx
+++ src/components/routes/introduction/Introduction.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 export const Introduction = () => {
   return <div className="introduction-page container row">
     <div className="col-12">
       <h3 className="mb-3">Introduction</h3>
-      <p>
-        React Testing Library (RTL) par Kent C. Dodds apporte une alternative à Enzyme de Airbnb.
+      <p className="mb-4">
+        <ArrowForwardIcon fontSize="small" /> React Testing Library (RTL) par Kent C. Dodds.
       </p>
-      <p></p>
-      <p>Contrairement à Enzyme qui test le fonctionnement interne d'un coposant.
-        RTL se concentre sur l'experience utilisateur. C'est à dire comment l'utilisateur interagierait avec le composant.
+      <p className="mb-1">
+        <ArrowForwardIcon fontSize="small" /> Experience utilisateur.
       </p>
-      <figure>
+      <figure className="mb-4">
         <blockquote className="blockquote">
           <p>The more your tests resemble the way your software is used, the more confidence they can give you.</p>
         </blockquote>
@@ -19,17 +19,14 @@ export const Introduction = () => {
           Guiding Principles in <cite title="Source Title">React Testing Library web site</cite>
         </figcaption>
       </figure>
-      <p>RTL encourage donc à ne pas tester les détails d'implémentation. Tel que :</p>
-      <ol>
-        <li>les methodes du composant</li>
-        <li>les states du composant</li>
-        <li>les composants enfants</li>
-        <li>le cycle de vie du composant</li>
-      </ol>
-      <p>RTL n'est pas une alternative à Jest, il a besoin de ce type de framework pour fonctionner.
-        Car il n'est pas un "test runner" ou un framework
+      <p className="mb-4">
+        <ArrowForwardIcon fontSize="small" />N'encourage pas à tester les détails d'implémentation.
       </p>
-      <p>Si vous utilisez create-react-app, React Testing Library est inclu par défaut.</p>
+      <p className="mb-4">
+        <ArrowForwardIcon fontSize="small" /> N'est pas un "test runner" ou un framework.
+      </p>
+      <p className="mb-4">
+        <ArrowForwardIcon fontSize="small" />React Testing Library est inclu dans create-react-app.</p>
     </div>
   </div>
 }
