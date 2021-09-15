de0fe2c33c4c43ad3c68fcbaa1ea44ada6d2fae5
Alicia Stotz <aliciastotz@gmail.com>
2021-09-15 15:15:54 +0200

    feat(conclusion): add source

--- src/components/routes/Conclusion/Conclusion.tsx
+++ src/components/routes/Conclusion/Conclusion.tsx
@@ -3,13 +3,20 @@ import React from 'react';
 import queryTab from './queryTab.png';
 
 export const Conclusion = () => {
-	return <div className="conclusion-page container row">
-		<div className="col-12">
-			<h3 className="mb-3">Conclusion</h3>
-			<div className="text-center">
-				<img src={queryTab} alt="Query table" width="65%" />
-			</div>
+  return <div className="conclusion-page container row">
+    <div className="col-12">
+      <h3 className="mb-3">Conclusion</h3>
+      <div className="text-center">
+        <img src={queryTab} alt="Query table" width="65%" /><br />
+        <a
+          className="small"
+          rel="noreferrer"
+          target="_blank"
+          href="https://testing-library.com/docs/react-testing-library/cheatsheet#queries">
+          (Source : React Testing web site - Cheatsheet)
+        </a>
+      </div>
 
-		</div>
-	</div>
+    </div>
+  </div>
 }
