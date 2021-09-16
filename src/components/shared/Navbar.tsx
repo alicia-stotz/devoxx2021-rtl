377d3ae8b1b626a6e900200559d8a1f8225e6584
Alicia Stotz <aliciastotz@gmail.com>
2021-09-16 16:42:05 +0200

    feat(navbar): add github link

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 export const Navbar = () => {
   return <nav className="navbar navbar-light">
     <div className="container-fluid">
       <Link className="navbar-brand" to="/">Devoxx 2021 - React Testing Library</Link>
+      <a
+        href="https://github.com/alicia-stotz/devoxx2021-rtl"
+        rel="noreferrer"
+        target="_blank">
+        <GitHubIcon />
+      </a>
     </div>
   </nav>
 };
