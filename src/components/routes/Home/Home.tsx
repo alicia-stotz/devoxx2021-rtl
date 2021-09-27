0480f53db21fd4e5811f24f6026f7ff2ec04c359
Alicia Stotz <aliciastotz@gmail.com>
2021-09-27 09:49:10 +0200

    feat(Home): add rtl logo

--- src/components/routes/Home/Home.tsx
+++ src/components/routes/Home/Home.tsx
@@ -1,12 +1,15 @@
+import octopus from './octopus.png';
+
 import TwitterIcon from '@material-ui/icons/Twitter';
 
 export const Home = () => {
-	return <div className="Home-page container row">
+	return <div className="Home-page container row text-center">
 		<div className="col-12">
-			<h1 className="display-2 mt-5 text-center">
+			<h1 className="display-2 mt-5">
 				A la découverte de Testing Library pour React
 			</h1>
-			<p className="lead text-center text-secondary mt-3">
+			<img src={octopus} alt="RTL octopus" /><br />
+			<p className="lead text-secondary mt-3">
 				<TwitterIcon /> @StotzAlicia
 			</p>
 		</div>
