d3bc810b7dfba0ca26671c9f3ac5d469a9217a7a
Alicia Stotz <aliciastotz@gmail.com>
2021-09-28 15:24:02 +0200

    feat(Pokedex): add comment in tests

--- src/components/routes/Examples/Pokedex.spec.tsx
+++ src/components/routes/Examples/Pokedex.spec.tsx
@@ -5,11 +5,14 @@ import { Pokedex } from "./Pokedex";
 describe("Pokedex page", () => {
   describe("Default page", () => {
     beforeEach(() => {
+      // Render the Pokedex component 
       render(<Pokedex />);
     });
 
     it("Should display 2 cards", () => {
+      // Multi select, selected card by text "Pokedex"
       const cards = screen.getAllByText("Pokedex");
+      // Count that selection is equal to 2
       expect(cards).toHaveLength(2);
     });
   });
